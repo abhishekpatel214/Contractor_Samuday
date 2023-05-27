3d63d52a6c2079972849a1c0e373d22213fc8f38
abhishek <86725705+abhishekpatel214@users.noreply.github.com>
2023-05-27 23:04:01 +0530

    enter profile done

--- app/src/main/java/com/example/contractor/activity_enter_profile.java
+++ app/src/main/java/com/example/contractor/activity_enter_profile.java
@@ -1,8 +1,10 @@
 package com.example.contractor;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class activity_enter_profile extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class activity_enter_profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_profile);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
